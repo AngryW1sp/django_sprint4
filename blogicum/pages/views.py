c6e4347e0e0f0caeb6c46cd74941009a23f0b895
AngryW1sp <147386694+AngryW1sp@users.noreply.github.com>
2024-06-07 21:01:04 +0300

    Моя попытка номер 100500

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -14,12 +14,15 @@ class Rules(TemplateView):
 
 
 def page_not_found(request, exception):
-    return render(request, 'pages/404.html', status=HTTPStatus.NOT_FOUND)
+    return render(request, 'pages/404.html',
+                  status=HTTPStatus.NOT_FOUND)
 
 
 def csrf_failure(request, reason=''):
-    return render(request, 'pages/403csrf.html', status=HTTPStatus.FORBIDDEN)
+    return render(request, 'pages/403csrf.html',
+                  status=HTTPStatus.FORBIDDEN)
 
 
 def server_error(request, exception=None):
-    return render(request, 'pages/500.html', status=HTTPStatus.INTERNAL_SERVER_ERROR)
+    return render(request, 'pages/500.html',
+                  status=HTTPStatus.INTERNAL_SERVER_ERROR)
